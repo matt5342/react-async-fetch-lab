5a7f26f5421986c34d48dc6a1215b7a21cf874bc
Matt <mattfsewell@gmail.com>
2021-03-17 17:03:16 -0400

    commit tree bff9512bfad6d588127374bd4dee14c13839ceb2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    state = {
+        peeps: []
+    }
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({
+                peeps: data.people
+            })
+        })
+    }
+    render (){
+        return(
+            <div><h1>{this.state.people}</h1></div>
+        )
+    }
+
+
+
+}
+export default App;
